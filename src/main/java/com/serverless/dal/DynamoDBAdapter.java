fb70e6ef9e288ef9add2ad50abc2775b6d67d799
michal <michaltartas@gmail.com>
2021-10-25 17:50:17 +0200

    backup code

---
+++ src/main/java/com/serverless/dal/DynamoDBAdapter.java
@@ -0,0 +1,8 @@
+package com.serverless.dal;
+
+public class DynamoDBAdapter {
+
+    private DynamoDBAdapter(){
+        this.client = AmazonDynamoDBClientBuilder.stadart().withRegion(Region.)
+    }
+}
